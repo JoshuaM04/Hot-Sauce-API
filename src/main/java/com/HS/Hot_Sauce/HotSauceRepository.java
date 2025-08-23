63b721c6b7a07db832a81fa2c29fa5bae957b7d5
Joshua Martinez <lmi25@txstate.edu>
2025-08-23 10:56:05 -0500

    Added respective methods to the HotSauceRepository which will be called from the HotSauceService class when searching for different queries. Furthermore, marked the getHotSauceByIndex as 'Optional' rather than 'List', as I expect one value at most to be returned due to it being a unique identifier.

--- src/main/java/com/HS/Hot_Sauce/HotSauceRepository.java
+++ src/main/java/com/HS/Hot_Sauce/HotSauceRepository.java
@@ -2,12 +2,19 @@ package com.HS.Hot_Sauce;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
+import java.util.List;
+
 
 @Repository
 public interface HotSauceRepository extends JpaRepository<HotSauce,Integer>
 {
     void deleteByIndex(int index);
+    // Optional, as I expect at most 1 value to be returned.
     Optional<HotSauce> findByIndex(int index);
+    List<HotSauce> findByName(String name);
+    List<HotSauce> findBySHU(String shu);
+    List<HotSauce> findBySL(String sl);
+    List<HotSauce> findByProducer(String producer);
+
 }
--- src/main/java/com/HS/Hot_Sauce/HotSauceService.java
+++ src/main/java/com/HS/Hot_Sauce/HotSauceService.java
@@ -1,6 +1,25 @@
 package com.HS.Hot_Sauce;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import java.util.List;
+import java.util.Optional;
+
 // Contains business logic & sits between the controller and repository
-public class HotSauceService {
+@Component
+public class HotSauceService
+{
+    private final HotSauceRepository hotSauceRepository;
+
+    @Autowired
+    public HotSauceService(HotSauceRepository hotSauceRepository) {this.hotSauceRepository = hotSauceRepository;}
+
+    public List<HotSauce> getHotSauce () {return hotSauceRepository.findAll();}
+
+    public Optional<HotSauce> getHotSauceByIndex (int searchIndex)
+    {
+        return hotSauceRepository.findByIndex(searchIndex);
+    }
+
 
 }
